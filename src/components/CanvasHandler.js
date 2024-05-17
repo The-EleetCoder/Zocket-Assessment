91764f87412cc47f29da3e24fae58bd006c8fe9e
The-EleetCoder <jaijain1803@gmail.com>
2024-05-17 23:51:32 +0530

    solved design_pattern overlapping bug

--- src/components/CanvasHandler.js
+++ src/components/CanvasHandler.js
@@ -11,12 +11,13 @@ export default class CanvasHandler {
     this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
   }
 
-  drawImage(url, x, y, width, height) {
+  drawImage(url, x, y, width, height, callback) {
     const img = new Image();
     img.crossOrigin = "anonymous";
     img.src = url;
     img.onload = () => {
       this.context.drawImage(img, x, y, width, height);
+      if (callback) callback();
     };
   }
 
@@ -63,57 +64,93 @@ export default class CanvasHandler {
       templateData.backgroundColor || this.defaultBackgroundColor
     );
 
+    // Drawing the design pattern first
     this.drawImage(
       templateData.urls.design_pattern,
       0,
       0,
       this.canvas.width,
-      this.canvas.height
-    );
-
-    if (userImage) {
-      this.drawUserImage(
-        userImage,
-        templateData.image_mask.x,
-        templateData.image_mask.y,
-        templateData.image_mask.width,
-        templateData.image_mask.height
-      );
-    } else {
-      this.drawImage(
-        templateData.urls.mask,
-        0,
-        0,
-        this.canvas.width,
-        this.canvas.height
-      );
-    }
-
-    this.drawImage(
-      templateData.urls.stroke,
-      0,
-      0,
-      this.canvas.width,
-      this.canvas.height
-    );
-
-    this.drawText(
-      templateData.caption.text,
-      templateData.caption.position.x,
-      templateData.caption.position.y,
-      templateData.caption.font_size,
-      templateData.caption.text_color,
-      templateData.caption.max_characters_per_line *
-        templateData.caption.font_size *
-        0.5
-    );
-    this.drawText(
-      templateData.cta.text,
-      templateData.cta.position.x,
-      templateData.cta.position.y,
-      30,
-      templateData.cta.text_color,
-      200
+      this.canvas.height,
+      () => {
+        // Drawing the user image
+        if (userImage) {
+          this.drawUserImage(
+            userImage,
+            templateData.image_mask.x,
+            templateData.image_mask.y,
+            templateData.image_mask.width,
+            templateData.image_mask.height
+          );
+          // Drawing the mask stroke after the user image
+          this.drawImage(
+            templateData.urls.stroke,
+            0,
+            0,
+            this.canvas.width,
+            this.canvas.height,
+            () => {
+              // Drawing the text elements
+              this.drawText(
+                templateData.caption.text,
+                templateData.caption.position.x,
+                templateData.caption.position.y,
+                templateData.caption.font_size,
+                templateData.caption.text_color,
+                templateData.caption.max_characters_per_line *
+                  templateData.caption.font_size *
+                  0.5
+              );
+              this.drawText(
+                templateData.cta.text,
+                templateData.cta.position.x,
+                templateData.cta.position.y,
+                30,
+                templateData.cta.text_color,
+                200
+              );
+            }
+          );
+        } else {
+          // Drawing the mask and then the mask stroke
+          this.drawImage(
+            templateData.urls.mask,
+            0,
+            0,
+            this.canvas.width,
+            this.canvas.height,
+            () => {
+              this.drawImage(
+                templateData.urls.stroke,
+                0,
+                0,
+                this.canvas.width,
+                this.canvas.height,
+                () => {
+                  // Drawing the text elements
+                  this.drawText(
+                    templateData.caption.text,
+                    templateData.caption.position.x,
+                    templateData.caption.position.y,
+                    templateData.caption.font_size,
+                    templateData.caption.text_color,
+                    templateData.caption.max_characters_per_line *
+                      templateData.caption.font_size *
+                      0.5
+                  );
+                  this.drawText(
+                    templateData.cta.text,
+                    templateData.cta.position.x,
+                    templateData.cta.position.y,
+                    30,
+                    templateData.cta.text_color,
+                    200
+                  );
+                }
+              );
+            }
+          );
+        }
+      }
     );
   }
 }
